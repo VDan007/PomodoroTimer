fad26ed8e4f84b4c6efc05bf697bf631875f270c
Dániel Varjaskéri <varjaskerid@gmail.com>
2023-01-02 15:57:37 +0100

    making butons work

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 let timeDisplay = document.querySelector(".timerNumber");
-let startingTime = 25;
-let startingTimeInSec = startingTime * 60;
+let pomodoroTime = 25;
+let shortBrake = 5;
+let longBrake = 15;
 let secPassed = 0;
 let minutesLeft ;
 let secondsLeft;
+let interval;
+let areWeCounting = false;
+let currentPeriod = "pomodoro";
+
+const startButton = document.getElementById("start");
+const pomodoroButton = document.getElementById("pomodoroBtn");
+const shortBrakeButton = document.getElementById("shortBrakeBtn");
+const longBrakeButton = document.getElementById("longBrakeBtn");
+startButton.addEventListener("click",startStopTimer);
+
+
+function startStopTimer(){
+    if(areWeCounting){
+        clearInterval(interval);
+        startButton.innerText = "Start";
+        areWeCounting = false;
+
+    }else{ 
+     interval = setInterval( update, 1000,pomodoroTime);
+     startButton.innerText = "Pause";
+     areWeCounting = true;
+    }
+
+     
+}
 
 
 
-function update (){
+function update (period){
+    let TimeInSec = period * 60;
     secPassed++;
-    minutesLeft =  Math.floor(  (startingTimeInSec - secPassed) / 60 );
-    secondsLeft = startingTimeInSec -(minutesLeft * 60) -secPassed ;
+    minutesLeft =  Math.floor(  (TimeInSec - secPassed) / 60 );
+    secondsLeft = TimeInSec -(minutesLeft * 60) -secPassed ;
     timeDisplay.innerText = " ";
     timeDisplay.innerText = `${minutesLeft < 10 ? "0"+minutesLeft : minutesLeft}:${secondsLeft < 10 ? "0"+secondsLeft : secondsLeft}`;
     document.title = `${minutesLeft < 10 ? "0"+minutesLeft : minutesLeft}:${secondsLeft < 10 ? "0"+secondsLeft : secondsLeft}`;
@@ -20,5 +47,3 @@ function update (){
 }
 
 
-
-let interval = setInterval( update, 1000);
