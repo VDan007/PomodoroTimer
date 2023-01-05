f17b82e0fca0c20b3dffc654da0d908e79084304
Dániel Varjaskéri <varjaskerid@gmail.com>
2023-01-05 08:34:18 +0100

    ad task starting to work

--- index.js
+++ index.js
@@ -109,6 +109,24 @@ setTimer(currentPeriod);
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskSettingsDiv = document.getElementById("taskSettings");
 const taskCancelBtn =document.getElementById("taskCancelBtn");
+const taskList = document.getElementById("taskList");
+
+function createTask(){
+    const svgHttp = 'http://www.w3.org/2000/svg';
+
+    const task = document.createElement('li');
+    const liLeft = document.createElement('div');
+    liLeft.classList.add('liLeft');
+    const checkIcon = document.createElementNS(svgHttp,'svg');
+    checkIcon.classList.add('taskOptionsIcon');
+    checkIcon.setAttribute('viewBox','0 0 512 512');
+    const path01 = document.createElementNS(svgHttp,'path');
+    path01.setAttribute('d','M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z');
+    checkIcon.appendChild(path01);
+    liLeft.appendChild(checkIcon);
+    task.appendChild(liLeft);
+    taskList.appendChild(task);
+}
 
 taskCancelBtn.addEventListener('click',()=> {
     taskSettingsDiv.classList.add('hideClass');
